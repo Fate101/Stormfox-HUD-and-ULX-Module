f1e2f90afbc4ca7067d0a7033a77cc454c7d7a64
Fate <HowdyImFate@gmail.com>
2022-05-11 03:54:44 +0100

    Made hud disappear when scoreboard is shown

--- lua/autorun/sh_stormfox-hud.lua
+++ lua/autorun/sh_stormfox-hud.lua
@@ -8,9 +8,7 @@ if CLIENT then
     CreateConVar("StormFoxHudServerNameEnabled",1,FCVAR_REPLICATED + FCVAR_SERVER_CAN_EXECUTE,"Enables/Disables Server Name text under Clock")
     surface.CreateFont( "StormFoxTime75" , { font = "Trebuchet24", size = 75, weight = 500 })
     surface.CreateFont( "StormFoxTime35" , { font = "Trebuchet24", size = 35, weight = 100 })
-      
-    
-    
+
     local function ShowStormFoxHUD()
         local time = nil
 
