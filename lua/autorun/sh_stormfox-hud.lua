febf910f11a941a6658e399eafb9d383ad4b9f41
Fate <HowdyImFate@gmail.com>
2022-05-15 05:26:17 +0100

    Added weather and temp to the hud

--- lua/autorun/sh_stormfox-hud.lua
+++ lua/autorun/sh_stormfox-hud.lua
@@ -1,30 +1,58 @@
 if SERVER then
     CreateConVar("StormFoxHudServerName","My Server Name",FCVAR_REPLICATED + FCVAR_SERVER_CAN_EXECUTE,"Sets the Server Name text under Clock")
     CreateConVar("StormFoxHudServerNameEnabled",1,FCVAR_REPLICATED + FCVAR_SERVER_CAN_EXECUTE,"Enables/Disables Server Name text under Clock")
+    CreateConVar("StormFoxHudWeatherEnabled",1,FCVAR_REPLICATED + FCVAR_SERVER_CAN_EXECUTE,"Enables/Disables Weather text under Clock")
+    CreateConVar("StormFoxHudTempEnabled",1,FCVAR_REPLICATED + FCVAR_SERVER_CAN_EXECUTE,"Enables/Disables Temp text under Clock")
 end
 
 if CLIENT then
     CreateConVar("StormFoxHudServerName","My Server Name",FCVAR_REPLICATED + FCVAR_SERVER_CAN_EXECUTE,"Sets the Server Name text under Clock")
     CreateConVar("StormFoxHudServerNameEnabled",1,FCVAR_REPLICATED + FCVAR_SERVER_CAN_EXECUTE,"Enables/Disables Server Name text under Clock")
+    CreateConVar("StormFoxHudWeatherEnabled",1,FCVAR_REPLICATED + FCVAR_SERVER_CAN_EXECUTE,"Enables/Disables Weather text under Clock")
+    CreateConVar("StormFoxHudTempEnabled",1,FCVAR_REPLICATED + FCVAR_SERVER_CAN_EXECUTE,"Enables/Disables Temp text under Clock")
     surface.CreateFont( "StormFoxTime75" , { font = "Trebuchet24", size = 75, weight = 500 })
-    surface.CreateFont( "StormFoxTime35" , { font = "Trebuchet24", size = 35, weight = 100 })
+    surface.CreateFont( "StormFoxTime40" , { font = "Trebuchet24", size = 40, weight = 100 })
+    surface.CreateFont( "StormFoxTime40o" , { font = "Trebuchet24", size = 40, weight = 100, outline = true })
 
     local function ShowStormFoxHUD()
         local time = nil
+        local info = ""
 
         if StormFox2 then
         	time = StormFox2.Time.TimeToString(nil, StormFox2.Setting.GetCache("12h_display"))
         elseif StormFox then
-    	time = StormFox.GetRealTime(nil, cookie.GetNumber("StormFox - ampm",0))
+    	    time = StormFox.GetRealTime(nil, cookie.GetNumber("StormFox - ampm",0))
         end
 
         if time == nil then return end
-        local x, y = draw.SimpleTextOutlined( time, "StormFoxTime75", 50, 40, color_white, 0, 0, .5, color_black )
+        local x, y = draw.SimpleTextOutlined( time, "StormFoxTime75", 50, 40, color_white, 0, 0, 1, color_black )
 
         if GetConVar("StormFoxHudServerNameEnabled"):GetInt() == 1 then
-            draw.SimpleTextOutlined( GetConVar("StormFoxHudServerName"):GetString() , "StormFoxTime35", 50 + 5, 50 + y/2 + 15, color_white, 0, 0, .5, color_black )
+            info = info .. GetConVar("StormFoxHudServerName"):GetString() .. "\n"
         end       
-        
+
+        if GetConVar("StormFoxHudWeatherEnabled"):GetInt() == 1 then
+            if StormFox2 then
+                info = info .. "Weather: " .. StormFox2.Weather.GetCurrent().Name .. "\n"
+            elseif StormFox then
+                info = info .. "Weather: " .. StormFox.GetWeatherID() .. "\n"
+            end
+        end
+
+        if GetConVar("StormFoxHudTempEnabled"):GetInt() == 1 then
+            if StormFox2 then
+                info = info .. "Temp: " .. StormFox2.Temperature.Get() .. "°C \n"
+            elseif StormFox then
+                info = info .. "Temp: " .. StormFox.GetTemperature() .. "°C \n"
+            end
+        end
+
+        if GetConVar("StormFoxHudTempEnabled"):GetInt() and GetConVar("StormFoxHudWeatherEnabled"):GetInt() and GetConVar("StormFoxHudServerNameEnabled"):GetInt() == 0 then
+            return 
+        else
+            draw.DrawText( info , "StormFoxTime40o", 50 + 5, 50 + y/2 + 15, color_white, TEXT_ALIGN_LEFT )
+        end
+
     end
     hook.Add("HUDPaint","EnableStormFoxHUDPaint",ShowStormFoxHUD) 
 
@@ -34,6 +62,7 @@ if CLIENT then
             hook.Remove("HUDPaint","EnableStormFoxHUDPaint")
         end
     end )
+    
     // When key is released
     hook.Add( "KeyRelease", "ReleaseScoreboard", function( ply, key )
         if ( key == IN_SCORE ) then
